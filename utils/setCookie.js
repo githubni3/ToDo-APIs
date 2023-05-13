0b89e8659a301e0a6536df3024fd09debeb8f502
Nitin Kumar Saxena <91969163+githubni3@users.noreply.github.com>
2023-05-13 16:23:47 +0530

    Update setCookie.js

--- utils/setCookie.js
+++ utils/setCookie.js
@@ -4,7 +4,7 @@ const token = jwt.sign({_id:user._id},process.env.JWT_SECRET);
 
     res.status(statusCode).cookie("token",token,{
         httpOnly:true,
-        maxAge:15*60*1000,
+        maxAge:60*1000,
         sameSite:process.env.NODE_ENV === "Development" ? "lax" : "none",
         secure:process.env.NODE_ENV === "Development" ? false : true,
     }).json({
